1710193916cb7f2c1af2b05c61a73fc836558316
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-04-23 14:21:22 -0700

    Menu - Display Categories

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -2,6 +2,7 @@ import Title from "./Title";
 import { useState } from "react";
 import menu from "./data";
 import Menu from "./Menu";
+import Categories from "./Categories";
 
 
 
@@ -12,11 +13,12 @@ item.category))];
 const App = () => {
  const [menuItems, setMenuItems] = useState(menu);
  const [categories, setCategories] = useState(allCategories);
- console.log(categories);
   return <main>
     <section className="menu">
       <Title text='our menu' />
+      <Categories categories={categories} />
       <Menu items={menuItems} />
+    
     </section>
   </main>;
 };
