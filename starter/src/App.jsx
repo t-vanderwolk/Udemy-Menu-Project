42868207730ca322fcebdddaca86a74f2db1e61a
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-04-23 17:47:14 -0700

    Menu - Filter Items

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -13,10 +13,19 @@ item.category))];
 const App = () => {
  const [menuItems, setMenuItems] = useState(menu);
  const [categories, setCategories] = useState(allCategories);
+ const filterItems = (category) => {
+// console.log(category);
+if (category === 'all') {
+  setMenuItems(menu);
+  return;
+}
+const newItems = menu.filter((item)=> item.category === category);
+setMenuItems(newItems);
+ };
   return <main>
     <section className="menu">
       <Title text='our menu' />
-      <Categories categories={categories} />
+      <Categories categories={categories} filterItems={filterItems} />
       <Menu items={menuItems} />
     
     </section>
